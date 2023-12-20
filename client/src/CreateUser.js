d900321d88420d7c6bf4356218f2cdfcb0720d01
Nitish sharma <sharmanitish183@gmail.com>
2023-12-20 18:48:46 +0530

    added ui for home page and createUser page

--- client/src/CreateUser.js
+++ client/src/CreateUser.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 export default function CreateUser() {
-  return (
-    <div>
-      Create user
-    </div>
-  )
+    return (
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded p-3'>
+                <form action="">
+                    <h2>Add User</h2>
+                    <div className="mb2">
+                        <label htmlFor="">Name</label>
+                        <input type="text" placeholder='Enter Name' className='form-control' />
+                    </div>
+                    <div className="mb2">
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder='Enter Email' className='form-control' />
+                    </div>
+                    <div className="mb2">
+                        <label htmlFor="">Age</label>
+                        <input type="number" placeholder='Enter Age' className='form-control' />
+                    </div>
+                    <button type="submit" className="btn btn-success mt-2">Create</button>
+                </form>
+            </div>
+        </div>
+
+    )
 }
--- client/src/Users.js
+++ client/src/Users.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Users() {
 
@@ -12,6 +13,7 @@ export default function Users() {
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
+            <Link to='/create' className='btn btn-success'>Add +</Link>
             <table className='table'>
                 <thead>
                     <tr>
