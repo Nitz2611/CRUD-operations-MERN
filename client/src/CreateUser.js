c2e068126445c6eab53b1631732a4cc0d8cced06
Nitish sharma <sharmanitish183@gmail.com>
2023-12-21 12:24:46 +0530

    esa desh hai mera

--- client/src/CreateUser.js
+++ client/src/CreateUser.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function CreateUser() {
+
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [age, setAge] = useState()
+
+    const Submit = (e) =>{
+        e.preventDefault();
+        axios.post("http://localhost:3001/createUser", {name, email, age})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-3'>
-                <form action="">
+                <form onSubmit={Submit}>
                     <h2>Add User</h2>
                     <div className="mb2">
                         <label htmlFor="">Name</label>
-                        <input type="text" placeholder='Enter Name' className='form-control' />
+                        <input type="text" placeholder='Enter Name' className='form-control'
+                        onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="mb2">
                         <label htmlFor="">Email</label>
-                        <input type="email" placeholder='Enter Email' className='form-control' />
+                        <input type="email" placeholder='Enter Email' className='form-control'
+                        onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="mb2">
                         <label htmlFor="">Age</label>
-                        <input type="number" placeholder='Enter Age' className='form-control' />
+                        <input type="number" placeholder='Enter Age' className='form-control'
+                        onChange={(e) => setAge(e.target.value)} />
                     </div>
                     <button type="submit" className="btn btn-success mt-2">Create</button>
                 </form>
